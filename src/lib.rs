37bc4e4cd1f062b34a13f90f330a230be4078d04
Marcelo Escobar <mers.escobar@gmail.com>
2024-07-09 20:18:57 -0300

    set terminal initialization

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::io::Stdout;
+
+use crossterm::{cursor::Hide, ExecutableCommand, terminal};
+use crossterm::cursor::Show;
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+
+pub fn setup_terminal(stdout: &mut Stdout) -> Result<(), Box<dyn Error>> {
+    terminal::enable_raw_mode()?;
+    
+    stdout
+        .execute(EnterAlternateScreen)?
+        .execute(Hide)?;
+        // .execute(SetForegroundColor(Color::DarkGrey))?
+        // .execute(SetBackgroundColor(Color::Black))?;
+
+    Ok(())
+}
+
+
+pub fn restore_terminal(stdout: &mut Stdout) -> Result<(), Box<dyn Error>> {
+    stdout
+        .execute(LeaveAlternateScreen)?
+        .execute(Show)?;
+        // .execute(ResetColor)?;
+    
+    terminal::disable_raw_mode()?;
+    
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io::{stdout, Write};
+
+use crossterm::{cursor, QueueableCommand};
+use crossterm::style::Print;
+use crossterm::terminal::{Clear, ClearType};
+use rs_battle_simulator::{restore_terminal, setup_terminal};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdout = stdout();
+
+    setup_terminal(&mut stdout).expect("Error starting the Terminal");
+
+
+    stdout
+        .queue(Clear(ClearType::All))?
+        .queue(cursor::MoveTo(10, 10))?
+        .queue(Print("Hello, world!\x0d\x0a"))?;
+
+    stdout.flush()?;
+
+
+    // out.queue(Clear(ClearType::All)).unwrap();
+    // out.queue(MoveTo(0, 0)).unwrap();
+    // out.flush().unwrap();
+
+
+    // restore_terminal(&mut stdout).expect("Error restoring the Terminal");
+    
+    Ok(())
 }
