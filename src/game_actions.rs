3825bf75460292d6a7fdae113151f8f86e42a51c
Marcelo Escobar <mers.escobar@gmail.com>
2024-07-18 19:01:17 -0300

    added fn select_opponent

--- src/game_actions.rs
+++ src/game_actions.rs
@@ -3,21 +3,21 @@ use std::io::{Stdout, Write};
 
 use crossterm::QueueableCommand;
 use crossterm::style::Print;
-
+use rand::Rng;
 use crate::game_ui::print_header;
 use crate::player::Player;
 
-pub fn select_player(players: &[Player], stdout: &mut Stdout) -> i32 {
+pub fn select_player(players: &[Player], stdout: &mut Stdout) -> usize {
     let mut user_input = String::new();
-    let mut selected_payer: i32;
+    let mut selected_payer: usize;
     let player_counter = players.len();
     let mut name: String = Default::default();
-    
+
     loop {
         print_header(stdout).expect("TODO: panic message");
 
         stdout.queue(Print("\x0d\x0aChoose your Player and go to the BATTLE field!\x0d\x0a\x0d\x0a")).expect("TODO: panic message");
-        
+
         players.iter().clone()
             .for_each(|p| {
                 name = format!("{} - {}\x0d\x0a", p.id, p.name);
@@ -31,9 +31,9 @@ pub fn select_player(players: &[Player], stdout: &mut Stdout) -> i32 {
 
         io::stdin().read_line(&mut user_input).expect("Error: unable to read user input.");
 
-        selected_payer = user_input.trim().parse().unwrap_or(0); 
-        
-        if selected_payer > 0 && selected_payer < player_counter as i32 {
+        selected_payer = user_input.trim().parse().unwrap_or(0);
+
+        if selected_payer > 0 && selected_payer <= player_counter {
             break;
         } else {
             user_input= "".to_string();
@@ -42,3 +42,32 @@ pub fn select_player(players: &[Player], stdout: &mut Stdout) -> i32 {
 
     selected_payer
 }
+
+pub fn select_opponent(players: &[Player], player_id: &usize) -> usize{
+    let mut selected_opponent: usize;
+
+    loop {
+        selected_opponent = rand::thread_rng().gen_range(1..=players.len());
+
+        if selected_opponent != *player_id {
+            break;
+        }
+    }
+    
+    selected_opponent
+}
+
+// function game_actions.select_opponent(players_list, player_id)
+// local selected_opponent = 0
+//
+// repeat
+// local opponent = math.random(#players_list)
+//
+// if opponent ~= player_id
+// then selected_opponent = opponent
+// end
+//
+// until selected_opponent > 0
+//
+// return selected_opponent
+// end
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 use std::io::{stdout};
 use rs_battle_simulator::players_parser::load_players;
-use rs_battle_simulator::game_actions::select_player;
+use rs_battle_simulator::game_actions::{select_opponent, select_player};
+use rs_battle_simulator::player::Player;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut stdout = stdout();
@@ -12,18 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     // setup_terminal(&mut stdout).expect("Error starting the Terminal");
+
+    let player_id = select_player(&players, &mut stdout);
+    let player: Player = players[player_id - 1].clone();
     
-    let _selected_player = select_player(&players, &mut stdout);
-
-    // stdout.flush()?;
-
-
+    let opponent_id = select_opponent(&players, &player_id);
+    let opponent: Player = players[opponent_id - 1].clone();
+    
+    
+    
+    
+    
     // out.queue(Clear(ClearType::All)).unwrap();
     // out.queue(MoveTo(0, 0)).unwrap();
     // out.flush().unwrap();
 
 
     // restore_terminal(&mut stdout).expect("Error restoring the Terminal");
-    
+
     Ok(())
 }
